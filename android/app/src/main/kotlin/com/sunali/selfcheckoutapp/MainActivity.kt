4ca9293550ca48a3cf17d57917b774bead73f5dc
ManzilKar <Manjil.Karmachary.Flutter@gmail.com>
2023-10-05 16:43:40 +0545

    jdsfj

---
+++ android/app/src/main/kotlin/com/sunali/selfcheckoutapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.manjil.onlinegrocery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/profile_avatar.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class Avatar extends StatefulWidget {
+  File imagePath;
+  String url;
+  Avatar({Key key, this.imagePath, this.url}) : super(key: key);
+  @override
+  _AvatarState createState() => _AvatarState();
+}
+
+class _AvatarState extends State<Avatar> {
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+        backgroundColor: Colors.white,
+        radius: 50,
+        backgroundImage: widget.url == null
+            ? widget.imagePath == null
+                ? null
+                : FileImage(widget.imagePath)
+            : NetworkImage(widget.url),
+        child: widget.imagePath == null
+            ? Icon(
+                Icons.person,
+                size: 50,
+                color: Colors.grey,
+              )
+            : CircleAvatar(backgroundImage: FileImage(widget.imagePath)));
+  }
+}
